986d907d75f3f2693e9dcf9e9efdf9ed115966f8
ShellyBrady <mchellebr@hotmail.com>
2022-09-22 00:37:29 +0000

    add alt text to img

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -203,11 +203,10 @@ function submit() {
 /**message on submit depending on score */
    if (score >= 3) {
     quizImage.src = "assets/images/welldone.jpg"
-    image.setAttribute("alt","Two happy dolphins");
+    image.alt = "Your text here"
     result = "Well done, you did great!";
    } else {
     quizImage.src = "assets/images/tryagain.jpg"
-    image.setAttribute("alt","Alitter of sad kittens");
     result = "Maybe you should try again!";
    }
 
